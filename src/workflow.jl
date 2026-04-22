4d41533f4833a4132ff4360361d2a61e3c1354a7
viniciuspiccoli <viniciuspiccoli2008@hotmail.com>
2026-04-22 15:25:28 -0300

    fix in workflow.jl to copy .ff using the flag ff_include.

--- src/workflow.jl
+++ src/workflow.jl
@@ -381,9 +381,21 @@ function step_npt!(wp::WorkflowParams, cycle::Int, data_file::String)
     cp(table_src, joinpath(ldir, wp.table_file); force=true)
 
     # Copy .ff include file (aluminosilicate)
-    if wp.is_alumino && !isempty(wp.ff_include)
-        ff_src = joinpath(wp.base_dir, wp.ff_include)
-        isfile(ff_src) && cp(ff_src, joinpath(ldir, basename(wp.ff_include)); force=true)
+  #  if wp.is_alumino && !isempty(wp.ff_include)
+  #      ff_src = joinpath(wp.base_dir, wp.ff_include)
+  #      isfile(ff_src) && cp(ff_src, joinpath(ldir, basename(wp.ff_include)); force=true)
+  #  end
+
+    # Copy .ff include file (if set)
+    if !isempty(wp.ff_include)
+        ff_src = isabs(wp.ff_include) ? wp.ff_include : joinpath(wp.base_dir, wp.ff_include)
+        ff_dst = joinpath(ldir, basename(wp.ff_include))
+        if isfile(ff_src)
+            cp(ff_src, ff_dst; force=true)
+            println("  [NPT] Copied $(basename(wp.ff_include)) → lammps/")
+        else
+            error("FF include file not found: $ff_src")
+        end
     end
 
 
